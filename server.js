941bf851d8f2936d6397601a8b82dce31ec41d4e
Suzan-Devpe <cooay318h@sina.com>
2020-12-31 10:02:50 +0800

    node

--- server.js
+++ server.js
@@ -1,31 +1,57 @@
 const http = require("http");
 
 const todos = [
-	{ id: 1, text: "Todo One" },
-	{ id: 2, text: "Todo Two" },
-	{ id: 1, text: "Todo Three" },
+  { id: 1, text: "Todo One" },
+  { id: 2, text: "Todo Two" },
+  { id: 1, text: "Todo Three" },
 ];
 
 const server = http.createServer((req, res) => {
-	res.writeHead(200, {
-		"Content-Type": "application/json",
-		"X-Powered-By": "Node.js",
-	});
+  const { method, url } = req;
 
-	let body = [];
-	req.on('data', chunk => {
-		body.push(chunk);
-	}).on('end', () =>{
-		body = Buffer.concat(body).toString();
-		console.log(body);
-	});
+  let body = [];
+  req
+    .on("data", (chunk) => {
+      body.push(chunk);
+    })
+    .on("end", () => {
+      let status = 404;
+      const response = {
+        success: false,
+        data: null,
+		error: null
+      };
+      body = Buffer.concat(body).toString();
 
-	res.end(
-		JSON.stringify({
-			success: true,
-			data: todos
-		}),
-	);
+      if (method === "GET" && url === "/todos") {
+        status = 200;
+        response.success = true;
+        response.data = todos;
+      } else if (method === "GET" && url === "/urmom") {
+        status = 200;
+        response.success = true;
+        response.data = "bruh, really that joke is old af mate";
+      } else if (method === "POST" && url === "/todos") {
+        const { id, text } = JSON.parse(body);
+
+        if (!id || !text) {
+          status = 400;
+		  response.error = "please add id and text";
+        } else {
+          todos.push({ id, text });
+          status = 201;
+          response.success = true;
+          response.data = todos;
+        }
+      }
+      res.writeHead(status, {
+        "Content-Type": "application/json",
+        "X-Powered-By": "Node.js",
+      });
+
+      res.end(JSON.stringify(response));
+    });
 });
+
 const PORT = 5000;
 server.listen(PORT, () => console.log(`Server running on port ${PORT}`));
